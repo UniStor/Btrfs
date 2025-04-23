dec172a44973220229a71c3e9814996135440a38
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-04-23 00:08:58 -0500

    Update addDisk.sh

--- Multiple device/Convert single disk to Raid/addDisk.sh
+++ Multiple device/Convert single disk to Raid/addDisk.sh
@@ -6,3 +6,5 @@ mkdir -p ${mountPoint}
 mount /dev/${firstDisk} ${mountPoint}
 
 btrfs device add /dev/${newDisk} ${mountPoint}
+
+#graph https://askubuntu.com/questions/1026334/convert-a-single-drive-btrfs-disk-into-a-raid1-with-a-second-disk
