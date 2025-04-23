a9c1d629499c90ba20fcb2d0593e994fcd8acae0
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-04-23 00:11:34 -0500

    Update addDisk.sh

--- Multiple device/Convert single disk to Raid/addDisk.sh
+++ Multiple device/Convert single disk to Raid/addDisk.sh
@@ -1,10 +1,14 @@
 firstDisk=vda2
 newDisk=vdb
 mountPoint=/mnt/data
+dataRaidType=single #raid1
+metadataRaidType=dup
 
 mkdir -p ${mountPoint}
 mount /dev/${firstDisk} ${mountPoint}
 
 btrfs device add /dev/${newDisk} ${mountPoint}
 
-#graph https://askubuntu.com/questions/1026334/convert-a-single-drive-btrfs-disk-into-a-raid1-with-a-second-disk
+btrfs balance start -dconvert=${dataRaidType} -mconvert=${metadataRaidType} ${mountPoint}
+
+# graph: https://askubuntu.com/questions/1026334/convert-a-single-drive-btrfs-disk-into-a-raid1-with-a-second-disk
